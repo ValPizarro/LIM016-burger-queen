b39d7c48fbd34225ee5423a386e6138cbe3e9362
ElianeRiveraCastillo22 <elianercast@gmail.com>
2022-03-05 13:15:09 -0500

    Se muestra las CardDish

--- src/components/utils/salonUtils/menuUtils/CardDish.jsx
+++ src/components/utils/salonUtils/menuUtils/CardDish.jsx
@@ -1,23 +1,37 @@
- import { onSnapshot, collection } from "firebase/firestore";
-import { useEffect } from "react";
+ import { onSnapshot, collection, doc } from "firebase/firestore";
+import { useEffect, useState } from "react";
 import  db from "../../../../firebase/config";
 
 const CardDish = () => {
-
-   useEffect(()=>{
-        onSnapshot(collection(db,"menu"),(snapshot)=>{
-            console.log(snapshot.docs);
-        });
-    });
+    const [menu, setMenu]=useState([{ name:"Loading..." ,id:"initial" }]);
+    console.log(menu)
+   useEffect(
+       ()=>
+        onSnapshot(collection(db,"menu"),(snapshot)=>
+            setMenu(snapshot.docs.map(doc =>({...doc.data(), id:doc.id})))
+        ),
+        []
+    );
 
 
     return (
         <div>
             <button>Almuerzo</button>
                 <div>
+
+                {menu.map((menu)=>(
+                    <>
+                    <div key={menu.id}>
                     <img src="" alt="" />
-                    <p>name</p>
-                    <p>precio</p>
+                    <p>{menu.name}</p>
+                    <p>{menu.price}</p>
+                    </div>
+                </>
+
+                ))}
+
+
+
                 </div>
         </div>
     )
