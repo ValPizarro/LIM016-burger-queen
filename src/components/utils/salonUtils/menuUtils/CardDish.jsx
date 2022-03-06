7947425c0d0182523b117d949503a57a0433c802
Valeria Pizarro <valpizarro.ce@outlook.com>
2022-03-06 11:09:38 -0500

    style: cardDish

--- src/components/utils/salonUtils/menuUtils/CardDish.jsx
+++ src/components/utils/salonUtils/menuUtils/CardDish.jsx
@@ -1,4 +1,4 @@
-// const Hamburguesa = require("../../../../img/menu/menu2/burgerDouble_V1.png");
+const Hamburguesa = require("../../../../img/menu/menu2/burgerDouble_V1.png");
 
 const CardDish = () => {
 //  const q = query (collection( db, "menu"), where(""))
@@ -7,14 +7,13 @@ const CardDish = () => {
 
     return (
       <div className="carDish">
-            <div className="photoDish">
-                <p>photo</p>
-                {/* <img src={Hamburguesa} alt="Food" /> */}
-            </div>
-            <div className="infoDish">
-                <p className="item1">{nameDish}</p>
-                <p className="item2">$ {priceDish}.00</p>
-            </div>
+        <div className="photoDish">
+          <img src={Hamburguesa} alt="Food" />
+        </div>
+        <div className="infoDish">
+          <p className="textInfo1">{nameDish}</p>
+          <p className="textInfo2">$ {priceDish}.00</p>
+        </div>
       </div>
     );
 }
--- src/components/utils/salonUtils/orderutils/ItemOrder.jsx
+++ src/components/utils/salonUtils/orderutils/ItemOrder.jsx
@@ -7,29 +7,29 @@ const Hamburguesa = require ("../../../../img/menu/menu2/burgerDouble_V1.png")
 const ItemOrderGeneral = () => {
     return (
       <div className="itemOrderBox">
-        <div className="firstDescriptionOrderBox">
-          <div className="item1">
+        <div className="descriptionOrderItem">
+          <div className="infoDish">
             <div className="photo">
               <img src={Hamburguesa} alt="Food" />
             </div>
-            <div className="description">
+            <div className="info">
               <p>Hamburguesa doble</p>
               <p> $15.00 </p>
             </div>
           </div>
-          <div className="item2">
+          <div className="noteOrder">
             <textarea placeholder="Indicaciones para el chef" />
           </div>
         </div>
 
-        <div className="secondDescriptionOrderBox">
-          <div className="item1">
+        <div className="buttonsOrderBox">
+          <div className="firstRowButtons">
             {/* <i class="fa-regular fa-pen-to-square"></i> */}
             <button className="fa-regular fa-square-plus"></button>
             <p>1</p>
             <button className="fa-regular fa-square-minus"></button>
           </div>
-          <div className="item2">
+          <div className="secondRowButtons">
             <p>$15.00 </p>
 
             <button class="fa-regular fa-trash-can"></button>
