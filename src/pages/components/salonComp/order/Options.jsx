452f394aafac27596e5957bdbe80b310353ad27c
Valeria Pizarro <valpizarro.ce@outlook.com>
2022-03-29 12:39:20 -0500

    Alerts

--- src/pages/components/salonComp/order/Options.jsx
+++ src/pages/components/salonComp/order/Options.jsx
@@ -1,4 +1,4 @@
-// import Swal from "sweetalert2";
+import Swal from "sweetalert2";
 
 export const Options = async ({ setOptionOrder }) => {
   // const {
@@ -14,33 +14,31 @@ export const Options = async ({ setOptionOrder }) => {
 
   /* inputOptions can be an object or Promise */
   /* inputOptions can be an object or Promise */
-  //   const inputOptions = new Promise((resolve) => {
-  //     setTimeout(() => {
-  //       resolve({
-  //         "#ff0000": "Red",
-  //         "#00ff00": "Green",
-  //         "#0000ff": "Blue",
-  //       });
-  //     }, 1000);
-  //   });
 
-  //   const { value: color } = await Swal.fire({
-  //     title: "Select color",
-  //     input: "radio",
-  //     inputOptions: inputOptions,
-  //     inputValidator: (value) => {
-  //       if (!value) {
-  //         return "You need to choose something!";
-  //       }
-  //     },
-  //   })
+    const inputOptions = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({
+          "#ff0000": "Red",
+          "#00ff00": "Green",
+          "#0000ff": "Blue",
+        });
+      }, 1000);
+    });
 
-  //   if (color) {
-  //     Swal.fire({ html: `You selected: ${color}` });
-  //   }
+    const { value: color } = await Swal.fire({
+      title: "Select color",
+      input: "radio",
+      inputOptions: inputOptions,
+      inputValidator: (value) => {
+        if (!value) {
+          return "You need to choose something!";
+        }
+      },
+    }).then(() => {
+      console.log(color);
+    });
 
-  //   return setOptionOrder(color);
-  // };
 
-console.log(setOptionOrder);
-}
+    return
+};
+
